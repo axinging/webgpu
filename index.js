157d8c5ce55b4c3240623416642cabe9e210dfa7
Xu Xing <xing.xu@intel.com>
2023-07-13 15:09:29 +0800

    Version 009: Support both Windows and Linux

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var nodeGPUBinding = require('bindings');
 var nodeGPU = (function() {
   var nodeGPUInstance;
   function createNodeGPU() {
-    const gpuProviderModule = nodeGPUBinding('dawn');
+    var isWin = process.platform === 'win32';
+    const gpuProviderModule =
+        isWin ? nodeGPUBinding('dawn_windows') : nodeGPUBinding('dawn_linux');
     const gpuProviderFlags = ['disable-dawn-features=disallow_unsafe_apis'];
     nodeGPUInstance = gpuProviderModule.create(gpuProviderFlags);
     return nodeGPUInstance;
